eb9aa7fc46d1e3dc0f88677ba8950ecbbc1957b5
Sadid <66120003+sadid369@users.noreply.github.com>
2024-09-21 02:25:21 +0600

    fixed app bar name of card screen

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -7,7 +7,7 @@ class CardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Card Screen'),
+        title: const Text('Card'),
       ),
       body: Container(),
     );
