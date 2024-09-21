573be3c953d1a25ede17275fa2b3355c5856d756
Sadid <66120003+sadid369@users.noreply.github.com>
2024-09-21 11:48:18 +0600

    updated

--- lib/screens/wallet_and_card_screen.dart
+++ lib/screens/wallet_and_card_screen.dart
@@ -19,12 +19,13 @@ class WalletAndCardScreen extends StatelessWidget {
           children: [
             Container(
               padding: const EdgeInsets.only(left: 20),
+              margin: const EdgeInsets.only(bottom: 5),
               alignment: Alignment.centerLeft,
               width: width,
               height: height * 0.05,
-              decoration: const BoxDecoration(
-                color: Color(0XffDCEBB8),
-                // border: Border.all(color: Colors.blue),
+              decoration: BoxDecoration(
+                color: const Color(0XffDCEBB8),
+                border: Border.all(color: Colors.black, width: 1),
               ),
               child: const Text(
                 'Wallet & Card',
@@ -34,71 +35,103 @@ class WalletAndCardScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Image.asset(
-              AppImage.creditCardPic,
-              width: width,
-              height: 300,
-              fit: BoxFit.contain,
+            Container(
+              height: 220,
+              margin: const EdgeInsets.only(bottom: 10),
+              child: Image.asset(
+                AppImage.creditCardPic,
+                width: width,
+                height: 300,
+                fit: BoxFit.contain,
+              ),
             ),
             Container(
-                padding: const EdgeInsets.all(5),
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                alignment: Alignment.center,
-                width: width,
-                height: 100,
-                decoration: BoxDecoration(
-                  color: const Color(0XffE1D7A3),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                // ignore: prefer_const_constructors
-                child: Column(
-                  children: const [
-                    Text(
-                      'Lifelinekart Prime Membership',
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              alignment: Alignment.center,
+              width: width,
+              height: 110,
+              decoration: BoxDecoration(
+                color: const Color(0XffE1D7A3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              // ignore: prefer_const_constructors
+              child: Column(
+                children: [
+                  const Text(
+                    'Lifelinekart Prime Membership',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '₹9999 / year',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                      Gap(50),
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                '₹3499/-',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'LifetimeActivate',
+                                style: TextStyle(
+                                  color: Color(0xff1B542A),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    alignment: Alignment.center,
+                    width: width,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: const Color(0xff035409),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: const Text(
+                      'Activate Prime Membership',
                       style: TextStyle(
-                        fontSize: 25,
+                        color: Colors.white,
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '₹9999 / year',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.lineThrough,
-                          ),
-                        ),
-                        Gap(50),
-                        Column(
-                          children: [
-                            Row(
-                              children: [
-                                Text(
-                                  '₹3499/-',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  'LifetimeActivate',
-                                  style: TextStyle(
-                                    color: Color(0xff1B542A),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        )
-                      ],
-                    ),
-                  ],
-                )),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 100,
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              alignment: Alignment.center,
+              width: width,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 1),
+              ),
+            ),
           ],
         ),
       ),
